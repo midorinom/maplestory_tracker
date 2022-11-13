a686717b12be8684344ea45d89ca0c7380858316
haofang <haofang97@gmail.com>
2022-11-13 17:10:53 +0800

    added heading

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Login.module.css";
 import { useAppSelector } from "../../store/hooks";
+import harmonySpirit from "../../images/harmony_spirit.png";
 import LoginComp from "./components/LoginComp";
 import Register from "./components/Register";
 
@@ -9,6 +10,10 @@ const Login = () => {
 
   return (
     <div className={`${styles.parent_ctn} + centered`}>
+      <div className={styles.header}>
+        <img className={styles.harmony_spirit} src={harmonySpirit} />
+        <h1 className={`${styles.header_text} + centered`}>Maple Tracker</h1>
+      </div>
       {page === "login" ? <LoginComp /> : <Register />}
     </div>
   );
