6be14083bc25a99679cfab3edec08a8bed1d717b
haofang <haofang97@gmail.com>
2022-11-14 01:40:47 +0800

    added background to dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <div className={styles.parent_ctn}>Dashboard</div>;
 };
 
 export default Dashboard;
