92d50400db8eb699ed56332e90b9d7cf77d40f0c
haofang <haofang97@gmail.com>
2022-12-11 17:06:25 +0800

    controlled checkbox

--- src/pages/Bossing/components/CheckboxCard.tsx
+++ src/pages/Bossing/components/CheckboxCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppDispatch } from "../../../store/hooks";
 import { FormControlLabel, Checkbox } from "@mui/material";
 import styles from "../Bossing.module.css";
@@ -10,13 +10,32 @@ interface CheckboxCardProps {
 }
 
 const CheckboxCard: React.FC<CheckboxCardProps> = (props) => {
+  // =========
+  // Variables
+  // =========
+  const [checked, setChecked] = useState<boolean>(props.checked);
+
+  // ==============
+  // Event Handlers
+  // ==============
+  function handleChange() {
+    if (checked) {
+      setChecked(false);
+    } else {
+      setChecked(true);
+    }
+  }
+
+  // ======
+  // Return
+  // ======
   return (
     <div>
       <FormControlLabel
         control={
           <Checkbox
-            //   onChange={props.handleDailiesChange}
-            checked={props.checked}
+            onChange={handleChange}
+            checked={checked}
             //   id={props.name}
           />
         }
